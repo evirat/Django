f57b1c110c2a6974596cb04448f2434ac99b4879
Edric VIRAT <etudiant@debian>
2023-05-05 09:03:29 +0200

    eee

--- computerApp/views.py
+++ computerApp/views.py
@@ -4,10 +4,10 @@ from computerApp.models import Machine
 # Create your views here.
 def index(request):
     machines = Machine.objects.all()
-    context = {'machines': machines}
+    context = {}
     return render(request, 'templates/index.html', context)
 
-def index(request):
+def machine_list(request):
     machines = Machine.objects.all()
     context = {'machines':machines}
     return render(request, 'computerApp/machine_list.html', context)
